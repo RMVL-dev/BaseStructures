90bb856bb8c5b72d25217728a14aefb746faade7
ramilmavlihanov <Ramil040201@gmail.com>
2024-12-01 18:22:27 +0400

    Initial commit

---
+++ task1.py
@@ -0,0 +1,20 @@
+#1st program
+print((9*5)**0.5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1001)
+
+#3rd program
+priority = 2*(2+2)
+not_priority = 2*2+2
+print(not_priority)
+print(priority)
+print(priority == not_priority)
+
+#4th program
+input_value = '123.456'
+array = input_value.split('.')
+print(array[1][0])
+
+
+
